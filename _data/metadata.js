2feb3a8f2842484c742610d07315c602c87f0acd
Thanh Tran <trongthanh@gmail.com>
2026-01-07 16:01:42 +0700

    current dotfiles

--- _data/metadata.js
+++ _data/metadata.js
@@ -3,9 +3,17 @@ module.exports = {
 	url: 'https://nuoidaycon.blog',
 	language: 'vi',
 	description: '',
+	logo: '',
+	facebook: 'https://www.facebook.com/deconlontunhien',
+	twitter: '',
 	author: {
 		name: 'Thảo Nguyễn',
 		email: 'fallgrassass@gmail.com',
 		url: 'https://nuoidaycon.blog',
+		bio: '',
+		image: '',
+		website: '',
+		facebook: '',
+		twitter: '',
 	},
 }
